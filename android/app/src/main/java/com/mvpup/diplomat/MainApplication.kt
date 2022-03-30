a2e972ae1445a83af35246da3b17eef22ecbbd4c
tencar <info@tencar.ru>
2022-03-30 16:51:03 +0300

    push interface

--- android/app/src/main/java/com/mvpup/diplomat/MainApplication.kt
+++ android/app/src/main/java/com/mvpup/diplomat/MainApplication.kt
@@ -4,11 +4,18 @@ import com.salesforce.androidsdk.app.SalesforceSDKManager
 import com.salesforce.androidsdk.rest.ClientManager
 import io.flutter.app.FlutterApplication
 import io.flutter.plugin.common.EventChannel
+import com.salesforce.androidsdk.push.PushNotificationInterface
+import android.util.Log
 
-class MainApplication : FlutterApplication() {
+class MainApplication : FlutterApplication() , PushNotificationInterface {
 
     override fun onCreate() {
         super.onCreate()
         SalesforceSDKManager.initNative(applicationContext, MainActivity::class.java)
+        SalesforceSDKManager.getInstance().setPushNotificationReceiver(this)
+    }
+    override fun onPushMessageReceived(data: Map<String, String>) {
+        Log.d("PUSH NOTY", data.toString())
+
     }
 }
